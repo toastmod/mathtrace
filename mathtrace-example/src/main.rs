50055ccaae7ac235e5e445712eaea8f1c62f256c
Andrew Numrich <toastymod@gmail.com>
2026-03-10 16:13:30 -0600

    Add function support

--- mathtrace-example/src/main.rs
+++ mathtrace-example/src/main.rs
@@ -1,5 +1,9 @@
 use mathtrace::*;
 
+fn do_stuff(x: i32) -> i32 {
+    x * 5
+}
+
 #[mathtrace]
 fn main() {
     let a = 1;
@@ -8,6 +12,8 @@ fn main() {
     let d = 1 + c;
     let e = 6 + 5;
     let f = d + e;
+    let g = do_stuff(f);
+    do_stuff(a) + 1;
 
     println!("Hello, world!");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,78 +1,71 @@
 use proc_macro::TokenStream;
-use quote::quote;
-use syn::{parse_macro_input, parse_quote, Expr, ExprBinary, ExprLit, ItemFn, Lit, Stmt};
+use quote::{quote, ToTokens};
+use syn::{parse_macro_input, parse_quote, Expr, ExprBinary, ExprCall, ExprLit, ItemFn, Lit, Stmt, Pat};
+
+/// Recursively build a trace string for any expression
+fn trace_expr(expr: &Expr) -> proc_macro2::TokenStream {
+    match expr {
+        Expr::Binary(bin) => {
+            let left_tokens = trace_expr(&bin.left);
+            let right_tokens = trace_expr(&bin.right);
+            let op = &bin.op;
+
+            quote! {
+                format!("({}) {} ({})", #left_tokens, stringify!(#op), #right_tokens)
+            }
+        }
+        Expr::Call(call) => {
+            let call_expr = quote! { #call };
+            quote! { format!("{} = {:?}", stringify!(#call_expr), #call_expr) }
+        }
+        Expr::Lit(ExprLit { lit: Lit::Int(_) | Lit::Float(_), .. }) => {
+            quote! { format!("{:?}", #expr) }
+        }
+        Expr::Path(_) => {
+            quote! { format!("{:?}", #expr) }
+        }
+        _ => {
+            // fallback for other expressions
+            let expr_tokens = quote! { #expr };
+            quote! { format!("{:?}", #expr_tokens) }
+        }
+    }
+}
 
 #[proc_macro_attribute]
 pub fn mathtrace(_attr: TokenStream, input: TokenStream) -> TokenStream {
     let mut func = parse_macro_input!(input as ItemFn);
-
     let mut new_stmts = Vec::new();
 
     for stmt in &func.block.stmts {
         match stmt {
-            // handle let statements
+            // let statements
             Stmt::Local(local) => {
                 if let Some(init) = &local.init {
-                    match init.expr.as_ref() {
-                        Expr::Binary(bin) => {
-                            let name = &local.pat;
-                            let left = &bin.left;
-                            let right = &bin.right;
-                            let op = &bin.op;
+                    let expr = &init.expr;
+                    let name = &local.pat;
 
-                            new_stmts.push(parse_quote! {
-                                println!(
-                                    "{} = {} {} {} = ({:?}) {} ({:?})",
-                                    stringify!(#name),
-                                    stringify!(#left),
-                                    stringify!(#op),
-                                    stringify!(#right),
-                                    #left,
-                                    stringify!(#op),
-                                    #right
-                                );
-                            });
-                        }
-                        Expr::Lit(ExprLit { lit: Lit::Int(_), .. }) |
-                        Expr::Lit(ExprLit { lit: Lit::Float(_), .. }) => {
-                            let name = &local.pat;
-                            let val = &init.expr;
+                    // generate trace
+                    let etrace = trace_expr(expr);
+                    let trace_stmt = parse_quote! {
+                        println!("{} = {}", stringify!(#name), #etrace);
+                    };
 
-                            new_stmts.push(parse_quote! {
-                                println!("{} = {:?}", stringify!(#name), #val);
-                            });
-                        }
-                        _ => {}
-                    }
+                    new_stmts.push(trace_stmt);
                 }
-
                 new_stmts.push(stmt.clone());
             }
 
-            // handle standalone expressions
+            // standalone expressions
             Stmt::Expr(expr, _) => {
+                // Only trace binary or call expressions
                 match expr {
-                    Expr::Binary(bin) => {
-                        let left = &bin.left;
-                        let right = &bin.right;
-                        let op = &bin.op;
-
-                        new_stmts.push(parse_quote! {
-                            println!(
-                                "{} {} {} = ({:?}) {} ({:?})",
-                                stringify!(#left),
-                                stringify!(#op),
-                                stringify!(#right),
-                                #left,
-                                stringify!(#op),
-                                #right
-                            );
-                        });
-                    }
-                    Expr::Lit(ExprLit { lit: Lit::Int(_) | Lit::Float(_), .. }) => {
-                        new_stmts.push(parse_quote! {
-                            println!("= {:?}", #expr);
-                        });
+                    Expr::Binary(_) | Expr::Call(_) => {
+                        let etrace = trace_expr(expr);
+                        let trace_stmt = parse_quote! {
+                            println!("{}", #etrace);
+                        };
+                        new_stmts.push(trace_stmt);
                     }
                     _ => {}
                 }
